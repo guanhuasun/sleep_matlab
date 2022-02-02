a999c449a52870584c9b32a58e931a2f40f852ef
guanhua <gs2658@nyu.edu>
2022-02-02 14:02:20 -0500

    some random updates

--- read_plot.m
+++ read_plot.m
@@ -1,19 +1,28 @@
 %read file
 N_neuron=1000;
-T=2000;
+lamda=0.01;
+n_conn=N_neuron^2*lamda;
+T=5000;
 write_dt=1;
 num_frame=ceil(T/write_dt);
-v_result=zeros(num_frame,N_neuron);
-cal_syn_result=zeros(num_frame,n_conn);
-ca_result=v_result;
-w_result=zeros(num_frame,n_conn);
-foldername="test_wake_w03/";
+v_data=zeros(num_frame,N_neuron);
+cal_syn_data=zeros(num_frame,n_conn);
+cal_data=v_data;
+w_data=zeros(num_frame,n_conn);
+
+theta_p=23;
+theta_d=theta_p-0.3; 
+tau=100;
+gamma_p=200;
+gamma_d=200;
+foldername=sprintf("n%up%ud%ugp%ugp%utau%u",N_neuron,10*theta_p,10*theta_d,gamma_p,gamma_d,tau);
+% foldername="n1";
 for ind_frame = 1:num_frame
     t_plot=ind_frame*write_dt;
     
-    xname=sprintf(foldername+'t%gx.mat',t_plot);
-    wname=sprintf(foldername+'t%gw.mat',t_plot);
-    calname=sprintf(foldername+'t%gcal.mat',t_plot);
+    xname=sprintf('t%gx.mat',t_plot);xname=fullfile(foldername,xname);
+    wname=sprintf('t%gw.mat',t_plot);wname=fullfile(foldername,wname);
+    calname=sprintf('t%gcal.mat',t_plot);calname=fullfile(foldername,calname);
     load(xname);
     load(wname);
     load(calname);
@@ -21,17 +30,17 @@ for ind_frame = 1:num_frame
     v=x(:,1);
     c_ca=x(:,6);
      
-    v_result(ind_frame,:)=v';
-    ca_result(ind_frame,:)=c_ca';
-    w_result(ind_frame,:)=weights';
-    cal_syn_result(ind_frame,:)=cal_syn;
+    v_data(ind_frame,:)=v';
+    cal_data(ind_frame,:)=c_ca';
+    w_data(ind_frame,:)=weights';
+    cal_syn_data(ind_frame,:)=cal_syn;
     %histogram(weights)
     %xlim([0,1])
     %drawnow
 
 end
 
-v_avg=sum(v_result,2)/N_neuron;
+v_avg=sum(v_data,2)/N_neuron;
 
 
     
--- sim_plot.m
+++ sim_plot.m
@@ -1,47 +1,61 @@
 %sim_plot
+close all
+spect_plot=0;
+hist_plot=0;
+
 t_vec=[write_dt:write_dt:T];
 figure
-ca_plot=plot(t_vec,ca_result(:,1:50:end),'LineWidth',1.5);
+cal_plot=plot(t_vec,cal_data(:,1:end),'LineWidth',1.5);
 grid on
 xlabel('Time/ms');
 ylabel('[Ca] of Neurons');
 set(gca,'FontSize',25)
 
 figure
-v_plot=plot(t_vec,v_result(:,1:50:end),'LineWidth',1.5);
+v_plot=plot(t_vec,v_data(:,1:end),'LineWidth',1.5);
 grid on
 xlabel('Time/ms');
 ylabel('V of Neurons');
 set(gca,'FontSize',25)
 
+figure
+v_fire=v_data'>0;
+imagesc(v_fire)
 
 figure
-v_plot=plot(t_vec,w_result(:,1:50:end),'LineWidth',1.5);
+v_plot=plot(t_vec,w_data(:,1:end),'LineWidth',1.5);
 grid on
 xlabel('Time/ms');
 ylabel('Synaptic Weights');
 set(gca,'FontSize',25)
 
 figure
-v_plot=plot(t_vec,sum(v_result,2)/1000,'LineWidth',1.5);
+v_plot=plot(t_vec,sum(v_data,2)/1000,'LineWidth',1.5);
 grid on
 xlabel('Time/ms');
 ylabel('Average Voltage');
 set(gca,'FontSize',25)
 
+if spect_plot
+    Fs = 1000/(write_dt);            % Sampling frequency                    
+    P = 1/Fs;             % Sampling period       
+    vs_data=v_avg(1:end/2,:);
+    L = length(vs_data);    % Length of signal
+    t_vec = (0:L-1)*P;        % Time vector
 
+    Y = fft(vs_data);
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
+    figure
+    plot(f(2:end),P1(2:end),'LineWidth',1.5) 
+    drawnow
+end
 
-Fs = 1000/write_dt;            % Sampling frequency                    
-P = 1/Fs;             % Sampling period       
-vs_data=v_avg(1:end,:);
-L = length(vs_data);    % Length of signal
-t_vec = (0:L-1)*P;        % Time vector
-
-Y = fft(vs_data);
-P2 = abs(Y/L);
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-f = Fs*(0:(L/2))/L;
-figure
-plot(f(1:end),P1(1:end),'LineWidth',1.5) 
-drawnow
+if hist_plot
+    for k = 1:100:99201
+    histogram(w_data(k,:))
+    drawnow
+    end
+end
